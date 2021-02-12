b6553d53599cd65d9e706f34956955267bad210d
okan <okangok76@gmail.com>
2021-02-12 20:56:28 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager : Program
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müsteri Id: " + customer.CustomerId);
+            Console.WriteLine("Müsteri ismi : " + customer.CustomerName);
+            Console.WriteLine("Müsteri Soy isim" + customer.CustomerLastName);
+            Console.WriteLine("Müsteri Numarası : " + customer.CustomerPhoneNumber);
+            Console.WriteLine("Müsteri Sehri : " + customer.CustomerCity);
+            Console.WriteLine("bilgiler kaydediliyor.");
+
+        }
+
+        public void Delete(Customer customer)
+        {
+            customer.CustomerId = customer.CustomerId;
+            customer.CustomerName = null;
+            customer.CustomerLastName = null;
+            customer.CustomerPhoneNumber = null;
+            customer.CustomerCity = null;
+
+            Console.WriteLine("Müsteri Id: " + customer.CustomerId + " numaralı müsteri silindi");
+            Console.WriteLine("Müsteri ismi : " + customer.CustomerName + " silindi");
+            Console.WriteLine("Müsteri Soy isim" + customer.CustomerLastName + " silindi");
+            Console.WriteLine("Müsteri Numarası : " + customer.CustomerPhoneNumber + " silindi");
+            Console.WriteLine("Müsteri Sehri : " + customer.CustomerCity + " silindi");
+        }
+        public void GetAll(Customer customer, Customer customer1, Customer customer2)
+        {
+            Customer[] customers1 = new Customer[] { customer, customer1, customer2 };
+
+
+            foreach (Customer custo in customers1)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Müsteri Id: " + custo.CustomerId);
+                Console.WriteLine("Müsteri ismi : " + custo.CustomerName);
+                Console.WriteLine("Müsteri Soy isim" + custo.CustomerLastName);
+                Console.WriteLine("Müsteri Numarası : " + custo.CustomerPhoneNumber);
+                Console.WriteLine("Müsteri Sehri : " + custo.CustomerCity);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1;
+            customer1.CustomerName = "Mert";
+            customer1.CustomerLastName = "yıldız";
+            customer1.CustomerPhoneNumber = "111111111111";
+            customer1.CustomerCity = "istanbul";
+
+            Customer customer2 = new Customer();
+            customer2.CustomerId = 2;
+            customer2.CustomerName = "Merve";
+            customer2.CustomerLastName = "İrem";
+            customer2.CustomerPhoneNumber = "22222222222";
+            customer2.CustomerCity = "Amasya";
+
+            Customer customer3 = new Customer();
+            customer3.CustomerId = 3;
+            customer3.CustomerName = "Okan";
+            customer3.CustomerLastName = "Gök";
+            customer3.CustomerPhoneNumber = "33333333333";
+            customer3.CustomerCity = "Ordu";
+
+            Customer customer4 = new Customer();
+            customer4.CustomerId = 3;
+            customer4.CustomerName = "Okan";
+            customer4.CustomerLastName = "Gök";
+            customer4.CustomerPhoneNumber = "33333333333";
+            customer4.CustomerCity = "Ordu";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.GetAll(customer1, customer2, customer3);
+            customerManager.Delete(customer3);
+            
+
+        }
+    }
+}
